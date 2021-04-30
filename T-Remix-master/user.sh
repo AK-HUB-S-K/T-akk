9eaa80772068e6883d29fd0d9cbb69c4793b5da1
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-04-30 13:44:14 +0530

    Add files via upload

--- T-Remix-master/user.sh
+++ T-Remix-master/user.sh
@@ -1,16 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 clear
-echo " "
-echo " "
-echo -e "\e[33mEnter Your Banner Name(e.g. AK) \e[32m \n\n"
-read varbanner
 
 echo " " 
 
-echo "toilet -f slant '           $varbanner'  -F gay | lolcat" > t-ban.txt
-
-echo -e "\e[33mEnter Your Terminal Name(e.g. root@ak) \e[32m  \n\n"
-read varterm
+echo "toilet -f slant '           AJAY'  -F gay | lolcat" > t-ban.txt
 
 echo " " 
 
@@ -18,7 +11,7 @@ echo "PS1='\[\e[1;34m
 \a┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
 \a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
 \a├─[\033[1;32m\w\033[1;34m]\033[1;34m
-\[\e[34m\]└─>$varterm[~]:#\[\e[1;92m\] ' " > ash.txt 
+\[\e[34m\]└─>root@ak[~]:#\[\e[1;92m\] ' " > ash.txt 
 
 cat "t-ban.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
@@ -27,4 +20,6 @@ cat "user.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 cat "ash.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 
 sleep 3
+
+
 
